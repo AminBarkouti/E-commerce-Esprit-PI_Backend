ed0f7903eca7559859c2cb79b8ff52d5cf783901
AminBarkouti <medaminbarkouti@gmail.com>
2024-05-16 14:48:34 +0100

    initialisation du projet

---
+++ src/main/java/tn/esprit/ecommerceespritpi/ECommerceEspritPiApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.ecommerceespritpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceEspritPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceEspritPiApplication.class, args);
+	}
+
+}
